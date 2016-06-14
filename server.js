98ee452d5952c43cafec41e9f977868f5a1e6597
Andrea Cuadra <apcuad@gmail.com>
2016-06-13 20:07:16 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express'); // web server application
-var http = require('http'); // http basics
-var app = express(); // instantiate express server
-var server = http.Server(app); // connects http library to server
-var io = require('socket.io')(server); // connect websocket library to server
+var express = require('express');       // web server application
+var http = require('http');             // http basics
+var app = express();                    // instantiate express server
+var server = http.Server(app);          // connects http library to server
+var io = require('socket.io')(server);  // connect websocket library to server
 var serialport = require('serialport'); // serial library
 var serverPort = 8000;
 var child_process = require('child_process');
